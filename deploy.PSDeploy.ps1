cfa61906ac0bc6c202e5419cd1e65c6f329959c2
Nhi <nhisoto@gmail.com>
2018-05-20 12:30:27 -0800

    !build

--- deploy.PSDeploy.ps1
+++ deploy.PSDeploy.ps1
@@ -5,7 +5,8 @@ if($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1)
             FromSource $ENV:BHProjectName
             To PSGallery
             WithOptions @{
-                ApiKey = "dac89b4c-a290-45aa-9cb1-7b16d3489505"
+                #ApiKey = "dac89b4c-a290-45aa-9cb1-7b16d3489505"
+                ApiKey = $ENV:NugetApiKey
             }
         }
     }
